b0742b914f0016cef92409e646043cf831e8f0aa
kuboy275 <caubebandu96@gmail.com>
2021-09-30 11:22:13 +0700

    tesst

---
+++ js/menu-script.js
@@ -0,0 +1,28 @@
+const btnOpenMenu = document.querySelector("#btnOpenMenu");
+const btnCloseMenu = document.querySelector("#btnCloseMenu");
+const overlayCloseMenu = document.querySelector("#overlayCloseMenu");
+const menuMobile = document.querySelector(".menu-mobile");
+
+btnOpenMenu.addEventListener("click", openMenu);
+btnCloseMenu.addEventListener("click", closeMenu);
+overlayCloseMenu.addEventListener("click", closeMenu);
+
+function openMenu() {
+    menuMobile.classList.add("show");
+    overlayCloseMenu.style.display = "block";
+}
+
+function closeMenu() {
+    menuMobile.classList.remove("show");
+    overlayCloseMenu.style.display = "none";
+}
+
+const showSub = document.querySelectorAll(".show-sub");
+showSub.forEach((e, i) => {
+    const btnCollapse = e.querySelector('.btnCollapse');
+    const subCollapse = e.querySelector('.collapse');
+    btnCollapse.addEventListener("click", () => {
+        btnCollapse.classList.toggle('rorate-icon-active');
+        subCollapse.classList.toggle('show');
+    })
+})
