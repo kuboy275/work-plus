baa2a6f27225ec23f500a2e3958970292cd48ffa
kuboy275 <caubebandu96@gmail.com>
2021-10-01 08:16:05 +0700

    SUCCESS

--- js/custom.js
+++ js/custom.js
@@ -1,32 +1,30 @@
-// START POPUP MENU
+$(document).ready(function() {
+    // START POPUP MENU
 
-const btnOpenMenu = document.querySelector("#btnOpenMenu");
-const btnCloseMenu = document.querySelector("#btnCloseMenu");
-const overlayCloseMenu = document.querySelector("#overlayCloseMenu");
-const menuMobile = document.querySelector(".menu-mobile");
+    $("#btnOpenMenu").on("click", openMenu);
+    $("#btnCloseMenu,#overlayCloseMenu").on("click", closeMenu);
 
-btnOpenMenu.addEventListener("click", openMenu);
-btnCloseMenu.addEventListener("click", closeMenu);
-overlayCloseMenu.addEventListener("click", closeMenu);
+    function openMenu() {
+        $('body').css('overflow', 'hidden');
+        $('.overlay-menu').fadeIn(300);
+        $('.menu-mobile').addClass('show');
+    }
 
-function openMenu() {
-    menuMobile.classList.add("show");
-    overlayCloseMenu.style.display = "block";
-}
+    function closeMenu() {
+        $('body').css('overflow', '');
+        $('.overlay-menu').fadeOut(300);
+        $('.menu-mobile').removeClass('show');
+    }
 
-function closeMenu() {
-    menuMobile.classList.remove("show");
-    overlayCloseMenu.style.display = "none";
-}
+    // const showSub = document.querySelectorAll(".show-sub");
+    // showSub.forEach((e, i) => {
+    //     const btnCollapse = e.querySelector('.btnCollapse');
+    //     const subCollapse = e.querySelector('.collapse');
+    //     btnCollapse.addEventListener("click", () => {
+    //         btnCollapse.classList.toggle('rorate-icon-active');
+    //         subCollapse.classList.toggle('show');
+    //     })
+    // })
 
-const showSub = document.querySelectorAll(".show-sub");
-showSub.forEach((e, i) => {
-    const btnCollapse = e.querySelector('.btnCollapse');
-    const subCollapse = e.querySelector('.collapse');
-    btnCollapse.addEventListener("click", () => {
-        btnCollapse.classList.toggle('rorate-icon-active');
-        subCollapse.classList.toggle('show');
-    })
-})
-
-// END POPUP MENU
+    // END POPUP MENU
+});
